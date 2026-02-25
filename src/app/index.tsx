0a2d02cd5714d43b17939cf01405c2785b2e1fd1
Lucas Daniel Tura <adevanirtura86@gmail.com>
2026-02-25 19:37:38 -0300

    PV

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,33 +6,42 @@ import { useState } from "react";
 import {
     Alert,
     Image,
-    KeyboardAvoidingView, Platform,
+    KeyboardAvoidingView,
+    Platform,
     ScrollView, StyleSheet, Text, View
 } from "react-native";
+
 export default function Index(){
     const [email, setEmail] = useState("");
 
     function handleSignIn(){
-        Alert.alert("Entrar", "Preencha o E-mail para entrar")
-        console.log (email)
+        console.log(email)
+        Alert.alert("Entrar", "Preencha e-mail e senha para entrar!")
     }
+
     return(
-            <KeyboardAvoidingView
-            style = {{flex:1}}
+        <KeyboardAvoidingView 
+            style={{flex:1}}
             behavior={Platform.select({ios:"padding", android:"height"})}
-            >
+        >
 
-        <ScrollView contentContainerStyle={{ flexGrow:1 }}
-                    showsVerticalScrollIndicator={false}>
-
+        <ScrollView 
+            contentContainerStyle={{ flexGrow:1 }}
+            showsVerticalScrollIndicator={false}
+        >
             <View style={styles.container}>
                 <Image 
                     source={require('@/assets/image1.png')}
                     style={styles.ilustration} 
                 />
+                <Text style={styles.title}>Entrar {email}</Text>
+                <Text style={styles.subtitle}>Acesse sua conta com e-mail e senha</Text>
                 <View style={styles.form}>
-                    <Input placeholder="E-mail"
-                    onChangeText={(text) => (email = text)} keyboardType="email-address" />
+                    <Input placeholder="E-mail" 
+                        keyboardType="email-address" 
+                        // onChangeText={(text) => console.log(text)}
+                        onChangeText={setEmail}
+                    />
                     <Input placeholder="Senha" secureTextEntry/>
                     <Button label="Entrar" onPress={handleSignIn} />
                     {/* <Button label="Entrar" style={{ backgroundColor: "green"}}/> */}
@@ -72,5 +81,12 @@ const styles = StyleSheet.create({
     form: {
         marginTop:24,
         gap:12
-    }
+    },
+    title:{ 
+        fontSize: 32,
+        fontWeight:900,
+    },
+    subtitle:{
+        fontSize:16,
+    },    
 })
--- src/app/signup.tsx
+++ src/app/signup.tsx
@@ -1,46 +1,42 @@
 import { Button } from "@/components/Button";
 import { Input } from "@/components/Input";
+
 import { Link } from "expo-router";
 import {
     Image,
-    KeyboardAvoidingView, Platform,
+    KeyboardAvoidingView,
+    Platform,
     ScrollView, StyleSheet, Text, View
 } from "react-native";
 
-
-export default function signup(){
+export default function Signup(){
     return(
         <KeyboardAvoidingView 
-            style = {{flex:1}}
-            behavior={Platform.select({ios:"padding", android:"height"})}
-            >
-
-       <ScrollView
-       contentContainerStyle={{ flexGrow:1 }}
-       showsVerticalScrollIndicator={false}>
-        
+                    style={{flex:1}}
+                    behavior={Platform.select({ios:"padding", android:"height"})}
+        >
+        <ScrollView 
+            contentContainerStyle={{ flexGrow:1 }}
+            showsVerticalScrollIndicator={false}
+        >
             <View style={styles.container}>
                 <Image 
-                    source={require('@/assets/image1.png')}
+                    source={require('@/assets/image2.png')}
                     style={styles.ilustration} 
                 />
-                <Text>
-                    Cadastrar
-                </Text>
-             <View >  
-                <Input placeholder="Nome Completo" />
-                <Input placeholder="E-mail" keyboardType="email-address" />
-                <Input placeholder="Senha" secureTextEntry/>
-                <Input placeholder="Confirmar Senha" secureTextEntry/>
-                </View> 
-                <Button
-                    label="Registrar"
-                    disabled={false}
-                    activeOpacity={0.7}
-                    />
-                <Text>Já tem uma conta?
+                <Text style={styles.title}>Cadastrar</Text>
+                <Text style={styles.subtitle}>Crie sua conta para acessar</Text>
+                <View style={styles.form}>
+                    <Input placeholder="Nome"  />
+                    <Input placeholder="E-mail" keyboardType="email-address" />
+                    <Input placeholder="Senha" secureTextEntry/>
+                    <Input placeholder="Confirmar Senha" secureTextEntry/>
+                    <Button label="Cadastrar" />
+                    {/* <Button label="Entrar" style={{ backgroundColor: "green"}}/> */}
+                </View>
+                <Text style={styles.footerText}>Já tem uma conta? 
                     <Link href="/" style={styles.footerLink}>
-                    {" "}Entre aqui
+                        {" "}Entre aqui
                     </Link>
                 </Text>
             </View>
@@ -69,7 +65,16 @@ const styles = StyleSheet.create({
     footerLink:{
         color:"#0929b8",
         fontWeight:700
-
     },
-
+    form: {
+        marginTop:24,
+        gap:12
+    },
+   title:{ 
+        fontSize: 32,
+        fontWeight:900,
+    },
+    subtitle:{
+        fontSize:16,
+    },    
 })
